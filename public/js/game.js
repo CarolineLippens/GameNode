64bad54c3a451b06921846dab83f4b1145d0bbfd
redouane <redouaneboukamir@gmail.com>
2019-02-27 10:48:54 +0100

    animation

--- public/js/game.js
+++ public/js/game.js
@@ -16,68 +16,111 @@ const config = {
       update: update
     } 
   };
-   
+  
+
 const game = new Phaser.Game(config);
- 
+let marche;
+
 function preload() {
   	
-  this.load.image('ship', 'assets/nain_champ/attack/attack1.png');
+  // this.load.image('ship', 'assets/nain_champ/attack/attack1.png');
+  this.load.spritesheet('dude','assets/bluerun.png', { frameWidth: 75, frameHeight: 80 });
+  this.load.spritesheet('dudeAttack','assets/bluerunAttack.png', { frameWidth: 107, frameHeight: 80 });
   this.load.image('otherPlayer', 'assets/minau_champ/attack/attack2.png');
-  this.load.image('star', 'assets/nain_champ/attack/attack4.png');  
+  // this.load.image('star', 'assets/nain_champ/attack/attack4.png');  
+  
 }
 
 function addPlayer(self, playerInfo) {
-  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y);
+    //  'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
   // if (playerInfo.team === '') {
   //   self.ship.setTint(0x0000ff);
   // } else {
   //   self.ship.setTint(0xff0000);
   // }
-  self.ship.setDrag(100);
-  self.ship.setAngularDrag(100);
-  self.ship.setMaxVelocity(200);
+  player.setDrag(100);
+  player.setAngularDrag(100);
+  player.setMaxVelocity(70);
 }
 
 
 
 function update() {  
-  if (this.ship) {
+  
+  if (player) {
     if (this.cursors.left.isDown) {
-      this.ship.setVelocityX(-80);
       
+      player.setVelocityX(-100);
+      player.anims.play('left', true);
+
     } else if (this.cursors.right.isDown) {
-      this.ship.setVelocityX(80);
+      
+      player.setVelocityX(100);
+      player.anims.play('right', true);
+
     } else if (this.cursors.down.isDown){
-      this.ship.setVelocityY(80);
+      player.setVelocityY(100);
+      player.anims.play('right', true);
     }else if (this.cursors.up.isDown){
-      this.ship.setVelocityY(-80);
+      player.setVelocityY(-100);
+      player.anims.play('right', true);
+    }
+    if(this.cursors.space.isDown){
+      player.anims.play('attack', true);
     }
 
   // emit player movement
-    var x = this.ship.x;
-    var y = this.ship.y;
-    var r = this.ship.rotation;
-    if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-      this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+    var x = player.x;
+    var y = player.y;
+    var r = player.rotation;
+    if (player.oldPosition && (x !== player.oldPosition.x || y !== player.oldPosition.y || r !== player.oldPosition.rotation)) {
+      this.socket.emit('playerMovement', { x: player.x, y: player.y, rotation: player.rotation });
     }
     
     // save old position data
-    this.ship.oldPosition = {
-      x: this.ship.x,
-      y: this.ship.y,
-      rotation: this.ship.rotation
+    player.oldPosition = {
+      x: player.x,
+      y: player.y,
+      rotation: player.rotation
 };
   
     
   }
 }
+
 function create() {
+
+  player = this.physics.add.sprite(100, 350, 'dude');
+  // player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  
+  this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 2 }),
+      frameRate: 10,
+      repeat: 1
+  });
+
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 3, end: 5 }),
+      frameRate: 10,
+      repeat: 1
+  });
+  this.anims.create({
+    key: 'attack',
+    frames: this.anims.generateFrameNumbers('dudeAttack', { start: 0, end: 2 }),
+    frameRate: 10,
+    repeat: 1
+});
 
   this.cursors = this.input.keyboard.createCursorKeys();
   var self = this;
 
   this.socket = io();
   this.otherPlayers = this.physics.add.group();
+
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
       if (players[id].playerId === self.socket.id) {
@@ -100,6 +143,7 @@ function create() {
       }
     });
   });
+  
   //Controle le mouvement de l'autre joueur
   this.socket.on('playerMoved', function (playerInfo) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
@@ -119,11 +163,11 @@ function create() {
     // Affichage des étoiles
   });
   this.socket.on('starLocation', function (starLocation) {
-    if (self.star) self.star.destroy();
-    self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
-    self.physics.add.overlap(self.ship, self.star, function () {
-      this.socket.emit('starCollected');
-    }, null, self);
+    // if (self.star) self.star.destroy();
+    // self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
+    // self.physics.add.overlap(self.ship, self.star, function () {
+    //   this.socket.emit('starCollected');
+    // }, null, self);
   });
 }
 
